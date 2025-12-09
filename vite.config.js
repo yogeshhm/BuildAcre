625635f349cf4f5e9579c705538b4cc724ac5c07
yogeshhm <yogeshhm958@gmail.com>
2025-12-09 11:58:04 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/BuildAcre/",
   plugins: [react()],
+  base: "/BuildAcre/",
 });
